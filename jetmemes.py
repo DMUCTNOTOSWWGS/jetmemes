58591f21f8ece8c9a239ee42ba5c799614206ece
James Houghton <jamesthoughton@gmail.com>
2017-03-01 13:25:40 -0500

    Meme beams can't steel fuel jet

--- jetmemes.py
+++ jetmemes.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import itertools
+from random import shuffle
 
 l = ['jet','fuel','melt','steel','beams']
-for a in itertools.permutations(l):
+perm = list(itertools.permutations(l))
+shuffle(perm)
+for a in perm:
     print("{} {} can't {} {} {}".format(a[0],a[1],a[2],a[3],a[4]))
-
