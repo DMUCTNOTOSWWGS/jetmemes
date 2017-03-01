680a25923378860d5c78557e7b7d66be0e74646b
James Houghton <jamesthoughton@gmail.com>
2017-03-01 13:41:36 -0500

    Beams jet can't meme steel fuel

---
+++ commit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# bash is best
+
+git commit -m "$(python3 createCommitMessage.py)"
+
+git push
--- jetmemes.py
+++ jetmemes.py
@@ -3,8 +3,25 @@
 import itertools
 from random import shuffle
 
-l = ['jet','fuel','melt','steel','beams']
-perm = list(itertools.permutations(l))
-shuffle(perm)
-for a in perm:
-    print("{} {} can't {} {} {}".format(a[0],a[1],a[2],a[3],a[4]))
+l = ['jet','fuel','melt','steel','beams', 'meme']
+
+def jet_melt():
+    perm = list(itertools.permutations(l))
+    shuffle(perm)
+    return perm
+
+def steel_fuel(a):
+    return "{}can't{}".format("{} "* 2," {}"*3).format(a[0][0].upper() + a[0][1:], *a[1:len(l)])
+
+def beam_memes():
+    perm = jet_melt()
+    print("\n".join([steel_fuel(a) for a in perm]))
+
+def jet_beams():
+    perm = jet_melt()[0]
+    return steel_fuel(perm)
+
+
+# A helpful alias
+def createCommitMessage():
+    return jet_beams()
